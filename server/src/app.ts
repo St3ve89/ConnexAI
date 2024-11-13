9af6c1a1c48b33c23ca1d680f086101c60c4e249
Istvan Acs <istvanacs@teamitg.com>
2024-11-13 12:31:30 +0100

    Fix the issue with authorization on metrics

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,12 +2,14 @@ import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import { metricsMiddleware } from "./services/metricsService.ts";
 import timeRoutes from "./routes/timeRoutes.ts";
+import { authMiddleware } from "./middlewares/authMiddleware.ts";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+app.use(authMiddleware);
 app.use(metricsMiddleware);
 
 app.use(timeRoutes);
--- server/src/services/metricsService.ts
+++ server/src/services/metricsService.ts
@@ -6,8 +6,4 @@ export const metricsMiddleware = promMid({
   requestDurationBuckets: [0.1, 0.5, 1, 1.5],
   requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
   responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
-  authenticate: (req) => {
-    const { authorization } = req.headers;
-    return authorization === "Bearer mysecrettoken";
-  },
 });
